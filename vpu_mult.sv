10a1dc3e5894752ddcfc0c4f666d136462c2a789
Metra8 <blascovalenti@gmail.com>
2026-03-19 18:33:15 +0100

    fixed latch in for loop

--- vpu_mult.sv
+++ vpu_mult.sv
@@ -1,3 +1,4 @@
+// vpu_mult.sv
 import vpu_pkg::*;
 
 module vpu_mult (
@@ -8,25 +9,61 @@ module vpu_mult (
     output vector_t      result_o
 );
 
+// Temporales para productos parciales declarados fuera del always_comb
+logic [15:0]  temp_uu_8  [16];
+logic [15:0]  temp_ss_8  [16];
+logic [15:0]  temp_su_8  [16];
+
+logic [31:0]  temp_uu_16 [8];
+logic [31:0]  temp_ss_16 [8];
+logic [31:0]  temp_su_16 [8];
+
+logic [63:0]  temp_uu_32 [4];
+logic [63:0]  temp_ss_32 [4];
+logic [63:0]  temp_su_32 [4];
+
+logic [127:0] temp_uu_64 [2];
+logic [127:0] temp_ss_64 [2];
+logic [127:0] temp_su_64 [2];
+
 always_comb begin
-    result_o = '0;
+    result_o    = '0;
+
+    // Inicializar todos los temporales
+    for (int i = 0; i < 16; i++) begin
+        temp_uu_8[i] = '0;
+        temp_ss_8[i] = '0;
+        temp_su_8[i] = '0;
+    end
+    for (int i = 0; i < 8; i++) begin
+        temp_uu_16[i] = '0;
+        temp_ss_16[i] = '0;
+        temp_su_16[i] = '0;
+    end
+    for (int i = 0; i < 4; i++) begin
+        temp_uu_32[i] = '0;
+        temp_ss_32[i] = '0;
+        temp_su_32[i] = '0;
+    end
+    for (int i = 0; i < 2; i++) begin
+        temp_uu_64[i] = '0;
+        temp_ss_64[i] = '0;
+        temp_su_64[i] = '0;
+    end
+
     case (sew_i)
 
         SEW8: begin
             for (int i = 0; i < 16; i++) begin
-                logic [15:0] temp_uu; // unsigned x unsigned
-                logic [15:0] temp_ss; // signed x signed
-                logic [15:0] temp_su; // signed x unsigned
-
-                temp_uu = vs2_i.i8b[i] * vs1_i.i8b[i];
-                temp_ss = signed'(vs2_i.i8b[i]) * signed'(vs1_i.i8b[i]);
-                temp_su = signed'(vs2_i.i8b[i]) * vs1_i.i8b[i];
+                temp_uu_8[i] = vs2_i.i8b[i] * vs1_i.i8b[i];
+                temp_ss_8[i] = signed'(vs2_i.i8b[i]) * signed'(vs1_i.i8b[i]);
+                temp_su_8[i] = signed'(vs2_i.i8b[i]) * vs1_i.i8b[i];
 
                 case (op_i)
-                    OPM_VMUL:    result_o.i8b[i] = temp_uu[7:0];
-                    OPM_VMULHU:  result_o.i8b[i] = temp_uu[15:8];   //high unsigned
-                    OPM_VMULH:   result_o.i8b[i] = temp_ss[15:8];   //high signed
-                    OPM_VMULHSU: result_o.i8b[i] = temp_su[15:8];   //high signed unsigned
+                    OPM_VMUL:    result_o.i8b[i] = temp_uu_8[i][7:0];
+                    OPM_VMULHU:  result_o.i8b[i] = temp_uu_8[i][15:8];
+                    OPM_VMULH:   result_o.i8b[i] = temp_ss_8[i][15:8];
+                    OPM_VMULHSU: result_o.i8b[i] = temp_su_8[i][15:8];
                     default:     result_o.i8b[i] = '0;
                 endcase
             end
@@ -34,19 +71,15 @@ always_comb begin
 
         SEW16: begin
             for (int i = 0; i < 8; i++) begin
-                logic [31:0] temp_uu;
-                logic [31:0] temp_ss;
-                logic [31:0] temp_su;
-
-                temp_uu = vs2_i.i16b[i] * vs1_i.i16b[i];
-                temp_ss = signed'(vs2_i.i16b[i]) * signed'(vs1_i.i16b[i]);
-                temp_su = signed'(vs2_i.i16b[i]) * vs1_i.i16b[i];
+                temp_uu_16[i] = vs2_i.i16b[i] * vs1_i.i16b[i];
+                temp_ss_16[i] = signed'(vs2_i.i16b[i]) * signed'(vs1_i.i16b[i]);
+                temp_su_16[i] = signed'(vs2_i.i16b[i]) * vs1_i.i16b[i];
 
                 case (op_i)
-                    OPM_VMUL:    result_o.i16b[i] = temp_uu[15:0];
-                    OPM_VMULHU:  result_o.i16b[i] = temp_uu[31:16];
-                    OPM_VMULH:   result_o.i16b[i] = temp_ss[31:16];
-                    OPM_VMULHSU: result_o.i16b[i] = temp_su[31:16];
+                    OPM_VMUL:    result_o.i16b[i] = temp_uu_16[i][15:0];
+                    OPM_VMULHU:  result_o.i16b[i] = temp_uu_16[i][31:16];
+                    OPM_VMULH:   result_o.i16b[i] = temp_ss_16[i][31:16];
+                    OPM_VMULHSU: result_o.i16b[i] = temp_su_16[i][31:16];
                     default:     result_o.i16b[i] = '0;
                 endcase
             end
@@ -54,19 +87,15 @@ always_comb begin
 
         SEW32: begin
             for (int i = 0; i < 4; i++) begin
-                logic [63:0] temp_uu;
-                logic [63:0] temp_ss;
-                logic [63:0] temp_su;
-
-                temp_uu = vs2_i.i32b[i] * vs1_i.i32b[i];
-                temp_ss = signed'(vs2_i.i32b[i]) * signed'(vs1_i.i32b[i]);
-                temp_su = signed'(vs2_i.i32b[i]) * vs1_i.i32b[i];
+                temp_uu_32[i] = vs2_i.i32b[i] * vs1_i.i32b[i];
+                temp_ss_32[i] = signed'(vs2_i.i32b[i]) * signed'(vs1_i.i32b[i]);
+                temp_su_32[i] = signed'(vs2_i.i32b[i]) * vs1_i.i32b[i];
 
                 case (op_i)
-                    OPM_VMUL:    result_o.i32b[i] = temp_uu[31:0];
-                    OPM_VMULHU:  result_o.i32b[i] = temp_uu[63:32];
-                    OPM_VMULH:   result_o.i32b[i] = temp_ss[63:32];
-                    OPM_VMULHSU: result_o.i32b[i] = temp_su[63:32];
+                    OPM_VMUL:    result_o.i32b[i] = temp_uu_32[i][31:0];
+                    OPM_VMULHU:  result_o.i32b[i] = temp_uu_32[i][63:32];
+                    OPM_VMULH:   result_o.i32b[i] = temp_ss_32[i][63:32];
+                    OPM_VMULHSU: result_o.i32b[i] = temp_su_32[i][63:32];
                     default:     result_o.i32b[i] = '0;
                 endcase
             end
@@ -74,19 +103,15 @@ always_comb begin
 
         SEW64: begin
             for (int i = 0; i < 2; i++) begin
-                logic [127:0] temp_uu;
-                logic [127:0] temp_ss;
-                logic [127:0] temp_su;
-
-                temp_uu = vs2_i.i64b[i] * vs1_i.i64b[i];
-                temp_ss = signed'(vs2_i.i64b[i]) * signed'(vs1_i.i64b[i]);
-                temp_su = signed'(vs2_i.i64b[i]) * vs1_i.i64b[i];
+                temp_uu_64[i] = vs2_i.i64b[i] * vs1_i.i64b[i];
+                temp_ss_64[i] = signed'(vs2_i.i64b[i]) * signed'(vs1_i.i64b[i]);
+                temp_su_64[i] = signed'(vs2_i.i64b[i]) * vs1_i.i64b[i];
 
                 case (op_i)
-                    OPM_VMUL:    result_o.i64b[i] = temp_uu[63:0];
-                    OPM_VMULHU:  result_o.i64b[i] = temp_uu[127:64];
-                    OPM_VMULH:   result_o.i64b[i] = temp_ss[127:64];
-                    OPM_VMULHSU: result_o.i64b[i] = temp_su[127:64];
+                    OPM_VMUL:    result_o.i64b[i] = temp_uu_64[i][63:0];
+                    OPM_VMULHU:  result_o.i64b[i] = temp_uu_64[i][127:64];
+                    OPM_VMULH:   result_o.i64b[i] = temp_ss_64[i][127:64];
+                    OPM_VMULHSU: result_o.i64b[i] = temp_su_64[i][127:64];
                     default:     result_o.i64b[i] = '0;
                 endcase
             end
